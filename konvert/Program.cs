2fe19012a89c23d8b2723d2c0f6ad04b2c6121ea
qctm0 <qctm0@DESKTOP-Q88PSBR>
2023-09-07 16:55:24 +0700

    Добавьте файлы проекта.

---
+++ konvert/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+Console.WriteLine("Vibirite vo chto perevesti");
+Console.WriteLine("esli iz celsius v fahrenheit vibirite 1");
+Console.WriteLine("esli iz fahrenheit v celsius vibirite 2");
+int gr = Convert.ToInt32(Console.ReadLine());
+double otvet = 0;
+switch (gr)
+{
+    case 1:
+        Console.WriteLine("Vvedite gradusi");
+        int C=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("otvet");
+        otvet = (C * 1.8) + 32;
+        Console.WriteLine(otvet);
+        break;
+    case 2:
+        Console.WriteLine("Vvedite gradusi");
+        int F = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("otvet");
+        otvet = (F-32)/1.8;
+        Console.WriteLine(otvet);
+        break;
+}
